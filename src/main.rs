614273b699bee654d3ad499db253ed0a2b34927c
Schweik7 <weibidewo0@gmail.com>
2023-03-16 16:55:13 +0800

    0.11

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,31 @@ async fn main(){
     let listener=TcpListener::bind("127.0.0.1:6379").await.unwrap();
     loop {
         let (socket,_)=listener.accept().await.unwrap();
-        process(socket).await;
+        // process(socket).await;
+        // `socket` 的所有权将被移动到新的任务中，并在那里进行处理
+        tokio::spawn(async move {
+           process(socket).await;
+        });
     }
 }
 
 async fn process(socket:TcpStream){
+    // let mut connection=Connection::new(socket);
+    // if let Some(frame)=connection.read_frame().await.unwrap(){
+    //     println!("GOT:{:?}",frame);
+    //     let response=Frame::Error("unimplemented".to_string());
+    //     connection.write_frame(&response).await.unwrap();
+    // }
+
+    use mini_redis::Command::{self,Get,Set};
+    use std::collections::HashMap;
+
+    let mut db=HashMap::new();
     let mut connection=Connection::new(socket);
-    if let Some(frame)=connection.read_frame().await.unwrap(){
-        println!("GOT:{:?}",frame);
-        let response=Frame::Error("unimplemented".to_string());
-        connection.write_frame(&response).await.unwrap();
+    while let Some(frame) = connection.read_frame().await.unwrap(){
+        let response = match Command::from_frame(frame).unwrap(); {
+
+            
+        };
     }
 }
